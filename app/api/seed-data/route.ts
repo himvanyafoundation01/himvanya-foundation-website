ba4ee04271403fc2fbfeaa2dd54311deb9775fab
ashish mehta <ashishmehtawork108@gmail.com>
2025-09-28 12:09:33 +0530

    update

--- app/api/seed-data/route.ts
+++
@@ -1,7 +0,0 @@
-import { Main } from "@/scripts/seed";
-import { NextResponse } from "next/server";
-
-export async function POST(req: Request) {
-    await Main()
-    return NextResponse.json({ message: "Data seeded successfully" })
-}
